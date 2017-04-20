b1f1afd10657a43c0e14561d535ce36aa0831ff5
agentsmith75002 <nicolas.lepinay@gmail.com>
2017-04-20 13:44:27 +0200

    First design

---
+++ MoneyTimer/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MoneyTimer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(400, 300);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        private void StartStopBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetBtn_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
